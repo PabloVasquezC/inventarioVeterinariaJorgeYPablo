2ae6f09e5373e8d56336f3417a147d4e24b69bab
PabloVasquez <vascor.pablo@gmail.com>
2024-11-28 19:19:13 -0300

    all merged

--- gestorProductos/models.py
+++ gestorProductos/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models 
+
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=50)
+    precio = models.IntegerField()
+    stock = models.IntegerField()
+    descripcion = models.TextField()
+    categoria = models.ForeignKey('Categoria', on_delete=models.CASCADE)
+    imagen = models.ImageField(upload_to='productos', null=True, blank=True)
+
+    def __str__(self):
+        return self.nombre
+    
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=50)
+    descripcion = models.TextField(default='')
+
+    def __str__(self):
+        return self.nombre
+    
