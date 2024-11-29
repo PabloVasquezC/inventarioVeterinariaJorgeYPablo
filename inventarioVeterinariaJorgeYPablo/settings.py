452246daa1948a12118117a9bd5492db0ba0f77f
PabloVasquez <vascor.pablo@gmail.com>
2024-11-29 15:11:28 -0300

    *

--- inventarioVeterinariaJorgeYPablo/settings.py
+++ inventarioVeterinariaJorgeYPablo/settings.py
@@ -74,15 +74,19 @@ TEMPLATES = [
 WSGI_APPLICATION = 'inventarioVeterinariaJorgeYPablo.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+import pymysql
+pymysql.install_as_MySQLdb()
+
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'loginVT',
+        'USER': 'root',
+        'PASSWORD': 'YO.7531.kurumi',
     }
 }
+
 
 
 
