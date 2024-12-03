bee5ed543cc1928734b591fe52a07d5a8a5bcdd1
PabloVasquez <vascor.pablo@gmail.com>
2024-12-02 22:08:39 -0300

    eliminar

--- gestorProductos/urls.py
+++ gestorProductos/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('lista_productos/', lista_productos, name='lista_productos'),
     path('agregar_producto/', agregar_producto, name='agregar_producto'),
     path('editar_producto/<int:id>/', editar_producto, name='editar_producto'),
+    path('eliminar_producto/<int:id>/', eliminar_producto, name='eliminar_producto'),
 ]
